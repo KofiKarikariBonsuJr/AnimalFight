3bd3568cea4f760e1590ee81f85a6efcdb1b6eb1
KofiKarikariBonsuJr <kkb@njit.edu>
2025-12-15 10:59:13 -0500

    fixed port

--- app/main.py
+++ app/main.py
@@ -417,4 +417,4 @@ def fight(body: FightRequest):
 # ------------------------------------------------------------------------------
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8001, log_level="info")
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, log_level="info")
